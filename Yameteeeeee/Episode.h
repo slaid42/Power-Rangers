54bf34dbde79f4c192435921b0e3eb428b38a2fc
Кощей Богдан <71867935+slaid42@users.noreply.github.com>
2021-12-04 19:38:25 +0300

    Update Episode.h

--- Yameteeeeee/Episode.h
+++ Yameteeeeee/Episode.h
@@ -1,25 +1,36 @@
+#pragma once
+#include <vector>
+#include "Scene.h"
+#include "Graphics.h"
 #include <list>
-#include "Episode.h"
-
+#include "Action.h"
 class Engine;
 
 class Episode
 {
-	
+
 private:
 	std::list<Scene*> scene_v;// âåêòîð ñöåí, äîáàâëåííûõ â ýïèçîä
 	const char* name;
-	Episode* next_episode;// ññûëêà íà ñëåäóþùèé ýïèçîä
+	Episode* next_episode;
+	Audio fon_music;
 public:
-	
+
 	Episode(const char* name_a);
 	void Next_episode(Episode* next_episode);// äîáàâèòü ññûëêó íà ñëåäóþùèé ýïèçîä
 	Scene* Add_scene(const char* name);//äîáàâèòü íîâóþ ñöåíó
+	
+	void AddFonAudio(const char* directive) {
+		fon_music = directive;
+	};
 
+	void PlayAudio() {
+		fon_music.Play();
+	}
 
 	std::list<Scene*>* get_scene_v()
 	{
-		return &scene_v; 
+		return &scene_v;
 	}
 
 	const char* get_name()
@@ -32,7 +43,7 @@ public:
 		return next_episode;
 	}
 
-	
+	~Episode();
 };
 
 
@@ -43,9 +54,17 @@ void Episode::Next_episode(Episode* next_episode_a)
 	next_episode = next_episode_a;
 }
 
-Scene* Episode::Add_scene( const char* name)
+Scene* Episode::Add_scene(const char* name)
 {
 	Scene* helper = new Scene(name);
 	scene_v.push_back(helper);
 	return helper;
+}
+
+Episode::~Episode()
+{
+	for (auto it = scene_v.begin(); it != scene_v.end(); ++it)
+	{
+		delete* it;
+	}
 }
