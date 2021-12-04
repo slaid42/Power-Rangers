4a548c9c96fe5bc4f1fce9a90882fe22aeee1f07
Кощей Богдан <71867935+slaid42@users.noreply.github.com>
2021-12-04 19:47:19 +0300

    Update Scene.h

--- Yameteeeeee/Scene.h
+++ Yameteeeeee/Scene.h
@@ -1,65 +1,70 @@
 #pragma once
-#include <vector>
-#include "Graphics.h"
-
-class Scene
+class Scene 
 {
 private:
+
+	
+
+
 	std::vector<Image*> scene_images;
-	std::vector<Game_text*> scene_texts;
-	std::vector < std::function<void(Characters&, ValuesHolder&)>>* actions;
+	std::vector<Game_text> scene_texts;
+	std::vector<Action> actions;
+	Scene* next_scene;
 	Audio music;
 	const char* name;
 
 public:
 	Scene(const char* name_a);
 
-	void Add_Background(char* id_name, char* direction, int screen_w, int screen_h);
-	void Add_image(char* id_name, char* direction, int x_a, int y_a, int w, int h);
-	void Add_text(char* id_name, char* content, const char* font, int size, SDL_Color color, SDL_Rect dest);
+	void Add_image(char* id_name, char* direction, int x_a, int y_a);
+	void Add_text(char* id_name, char* content);
 	void Add_Choice(Scene* sc1, Scene* sc2);
 	void Add_Action(std::function<void(Characters&, ValuesHolder&)> action);
 
+	void Next_Scene(Scene* next_scene_arg)
+	{
+		
+		Action action(SDL_MOUSEBUTTONDOWN, [next_scene_arg](Characters& ch, ValuesHolder& vh, Scene* scene, SDL_Event eventt) -> EngineInstruction
+			{
+				scene->Next_Scene(next_scene_arg, "slyshen_krik_na_vsy_dolgopy");
+				return EngineInstruction("stop_scene");
+			});
+		actions.push_back(action);
+	} 
+	
+	void Next_Scene(Scene* scene, const char* password)
+	{
+		if (password == "slyshen_krik_na_vsy_dolgopy")
+		{
+			next_scene = scene;
+		}
+	}
 
-	std::vector<Image*> get_scene_images()
+	const char* get_name()
+	{
+		return name;
+	}
+	std::vector<Action>& get_actions()
+	{
+		return actions;
+	}
+	Scene* get_next_scene()
+	{
+		return next_scene;
+	}
+	std::vector<Image*>& get_scene_images()
 	{
 		return scene_images;
 	}
-
-
-	~Scene()
-	{
-		delete actions;
-	}
-
-
+	
 
 };
 
 
-Scene::Scene(const char* name_a): name(name_a), scene_images(), scene_texts(){}
+Scene::Scene(const char* name_a): name(name_a), scene_images(), scene_texts(), next_scene(nullptr) {}
 
-void Scene::Add_Background(char* id_name, char* direction, int screen_w, int screen_h){
-    scene_images.push_back(new Image(id_name, direction, 0, 0, screen_w, screen_h));
-}
-
-void Scene::Add_text(char* id_name, char* content, const char* font, int size, SDL_Color color, SDL_Rect dest, screen_w){
-    SDL_Surface *sDest;
-    for(uint16_t i=0; i++; i <= sizeof(content)*size/screen_w){
-        char* text = content.substr(i*screen_w, (1+i)screen_w);
-        TTF_Font *fnt = TTF_OpenFont(font, size);
-        SDL_Surface *sText = TTF_RenderText_Blended(fnt, text, color);
-        SDL_BlitSurface( sText, NULL, dest, &dest );
-        dest.y+=size;
-    }
-    text1 = new Image(id_name, NULL, SDL_Rect.x, SDL_Rect.y, SDL_Rect.w, SDL_Rect.h));
-    text1.surf = sDest;
-    scene_images.push_back(text1);
-    TTF_CloseFont(fnt);
-}
-
-void Scene::Add_image(char* id_name, char* direction, int x_a, int y_a, int w, int h)
+void Scene::Add_image(char* id_name, char* direction, int x_a, int y_a)
 {
-
-	scene_images.push_back(new Image(id_name, direction, x_a, y_a, w, h));
+	
+	scene_images.push_back(new Image(id_name, direction, x_a, y_a));
 }
