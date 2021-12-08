67a2e678bdecfd8d3af82c63d3ff1b91745fb495
Kuzin Rostislav <kuzin.rf@phystech.edu>
2021-12-09 00:45:09 +0700

    A?

--- Yamete/Yamete/Graphics.h
+++ Yamete/Yamete/Graphics.h
@@ -1,14 +1,80 @@
 #pragma once
+
+void DrawImg(SDL_Surface *img, int x, int y, int w, int h, SDL_Surface *screen){
+    SDL_Rect dest;
+    dest.x = x;
+    dest.y = y;
+    dest.w = w;
+    dest.h = h;
+    SDL_BlitScaled(img, NULL, screen, &dest);
+}
+
+int min(int l, int r) {
+    if (l < r) {
+        return l;
+    }
+    else { return r; }
+}
+
+char* CharSlipt(const char* content, int l, int r) {
+    std::string s(content);
+    if(l >= s.size()){return " ";}
+    std::string subs = s.substr(l, min( s.size() - l, r - l));
+    int len = min((r - l - 1),(s.size() - l - 1));
+    char* res = new char[len];
+    strcpy(res, subs.c_str());
+    std::cout << res << std::endl;
+    return res;
+}
+
+void PrintText(SDL_Surface *sDest, const char* message, const char* font, int size, SDL_Color color, SDL_Rect dest){
+    TTF_Font *fnt = TTF_OpenFont(font, size);
+    SDL_Surface *sText = TTF_RenderText_Blended(fnt, message, color);
+    SDL_BlitSurface( sText, NULL, sDest, &dest );
+    SDL_FreeSurface( sText );
+    TTF_CloseFont(fnt);
+}
+
+void DrawSimpleButton(SDL_Surface* screen, int x, int y, int w, int h, const char* text, const char* font, int siz, int red, int green, int blue, int bred, int bgreen, int bblue){
+    SDL_Rect dest;
+    dest.x = x;
+    dest.y = y;
+    dest.w = w;
+    dest.h = h;
+    SDL_FillRect(screen, &dest, SDL_MapRGB(screen->format, bred, bgreen, bblue));
+    dest.x += siz/10;
+    dest.y += siz/10;
+    SDL_Color clr;
+    clr.r = red;
+    clr.g = green;
+    clr.b = blue;
+    PrintText(screen, text, font, siz, clr, dest);
+}
+
+void DrawImageButton(SDL_Surface* screen, SDL_Surface* img, int x, int y, int w, int h, const char* text, const char* font, int siz, int red, int green, int blue){
+    SDL_Rect dest;
+    dest.x = x;
+    dest.y = y;
+    dest.w = w;
+    dest.h = h;
+    //(SDL_Surface *img, int x, int y, SDL_Surface *screen)
+    DrawImg(img, x, y, w, h, screen);
+    dest.x += siz/10;
+    dest.y += siz/10;
+    SDL_Color clr;
+    clr.r = red;
+    clr.g = green;
+    clr.b = blue;
+    PrintText(screen, text, font, siz, clr, dest);
+}
+
 class Image// класс картинки
 {
 private:
-
-
 	SDL_Surface* surf;
 	int x, y;
 	char* directive;
 	char* key;
-
 
 public:
 
@@ -41,7 +107,7 @@ public:
 		}
 	}
 
-	
+
 };
 class Game_text
 {
--- Yamete/Yamete/Scene.h
+++ Yamete/Yamete/Scene.h
@@ -1,109 +1,32 @@
 #pragma once
-
-class Episode;
-class Scene 
+class Scene
 {
 private:
 
 	std::vector<Image*> scene_images;
 	std::vector<Game_text> scene_texts;
-	std::list<Action> actions;
-	std::list<Action>::iterator action_list_pos;
-	Episode* my_episode;
+	std::vector<Action> actions;
 	Scene* next_scene;
 	Audio music;
 	const char* name;
 
-	void change_alp(std::list<Action>::iterator arg)
-	{
-		action_list_pos = arg;
-	}
-
-	void change_my_episode(Episode* epi)
-	{
-		my_episode = epi;
-
-
-	}
-
 public:
-
-	friend Game_Engine;
-	friend Episode;
-
 	Scene(const char* name_a);
 
 	void Add_image(char* id_name, char* direction, int x_a, int y_a);
 	void Add_text(char* id_name, char* content);
-
-	std::list<Action>::iterator get_alp()
-	{
-		return action_list_pos;
-	}
-
-	void Add_Action(SDL_EventType event_type, std::function<EngineInstruction(ActionValues& act_val)> action_arg)
-	{
-		Action action(event_type, action_arg);
-		push_action(action);
-	}
-	void Add_Script(std::function<EngineInstruction(ActionValues& act_val)> scr)
-	{
-		Action action(NULL_EVENT.type, scr);
-		push_action(action);
-	}
-
-	void push_action(Action& action)
-	{
-		if (actions.size() < 1)
-		{
-			actions.push_back(action);
-		}
-		else
-		{
-			actions.emplace(action_list_pos, action);
-		}
-	}
+	void Add_Choice(Scene* sc1, Scene* sc2);
+	void Add_Action(std::function<void(Characters&, ValuesHolder&)> action);
 
 	void Next_Scene(Scene* next_scene_arg)
 	{
-		
-		Action action(NULL_EVENT.type,[next_scene_arg](ActionValues& act_val) -> EngineInstruction
+
+		Action action(SDL_MOUSEBUTTONDOWN, [next_scene_arg](Characters& ch, ValuesHolder& vh, Scene* scene, SDL_Event eventt) -> EngineInstruction
 			{
-				act_val.this_scene->Next_Scene(next_scene_arg, "slyshen_krik_na_vsy_dolgopy");
+				scene->Next_Scene(next_scene_arg, "slyshen_krik_na_vsy_dolgopy");
 				return EngineInstruction("stop_scene");
 			});
-		push_action(action);
-
-	} 
-
-	
-	void Add_Clicker()
-	{
-		Action action(SDL_MOUSEBUTTONDOWN, [](ActionValues& act_val)-> EngineInstruction
-			{
-				return EngineInstruction("continue");
-				//усё, кликер ничего не делает
-			});
-		push_action(action);
-	}
-
-	void Add_Choice(Scene* sc1, Scene* sc2)
-	{
-		Action action(SDL_MOUSEBUTTONDOWN, [sc1, sc2](ActionValues& act_val)-> EngineInstruction
-			{
-				if (act_val.event.button.button == SDL_BUTTON_LEFT)
-				{
-					act_val.this_scene->Next_Scene(sc1);
-					return EngineInstruction("continue");
-				}
-				if (act_val.event.button.button == SDL_BUTTON_RIGHT)
-				{
-					act_val.this_scene->Next_Scene(sc2);
-					return EngineInstruction("continue");
-				}
-				return EngineInstruction("nothing");
-			});
-		push_action(action);
+		actions.push_back(action);
 	}
 
 	void Next_Scene(Scene* scene, const char* password)
@@ -118,7 +41,7 @@ public:
 	{
 		return name;
 	}
-	std::list<Action>& get_actions()
+	std::vector<Action>& get_actions()
 	{
 		return actions;
 	}
@@ -130,21 +53,15 @@ public:
 	{
 		return scene_images;
 	}
-	
-	Episode* get_my_episode()
-	{
-		return my_episode;
-	}
+
+
 };
 
 
-Scene::Scene(const char* name_a): name(name_a), scene_images(), scene_texts(), next_scene(nullptr), actions (), action_list_pos()
-{
-	action_list_pos = actions.end();
-}
+Scene::Scene(const char* name_a): name(name_a), scene_images(), scene_texts(), next_scene(nullptr) {}
 
 void Scene::Add_image(char* id_name, char* direction, int x_a, int y_a)
 {
-	
+
 	scene_images.push_back(new Image(id_name, direction, x_a, y_a));
 }
