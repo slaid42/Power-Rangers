9765e5d413f7123aee5b2f3730e1e0fbcd68de91
Кощей Богдан <71867935+slaid42@users.noreply.github.com>
2021-12-08 14:01:35 +0300

    Update Characters.h

--- Yameteeeeee/Characters.h
+++ Yameteeeeee/Characters.h
@@ -1,29 +1,25 @@
 #pragma once
 #include <SDL.h>
 #include "Graphics.h"
+#include <unordered_map>
 
 class Character {
 private:
 	const char *name;
 	SDL_Color color;
-	Image image;
+	std::map<const char*, Image> im_holder;
 public:
-	Character(const char* _name, SDL_Color _color, Image _image) {
+	Character(const char* _name, SDL_Color _color) {
 		name = _name;
 		color = _color;
-		image = _image;
 	}
 	Character(const Character& other) {
 		name = other.name;
 		color = other.color;
-		image = other.image;
+		im_holder = other.im_holder;
 	}
-	Character(Character& other) = delete;
 	void Change_Color(SDL_Color other) {
 		color = other;
-	}
-	void Change_Image(Image other) {
-		image = other;
 	}
 	~Character() {
 		delete name;
@@ -31,20 +27,22 @@ public:
 	Character& operator=(const Character& other) {
 		name = other.name;
 		color = other.color;
-		image = other.image;
+		im_holder = other.im_holder;
 		return *this;
 	};
-	Character& operator=(Character& other) = delete;
-	const char* get_name(){
+	const char* get_name() {
 		return name;
 	}
-	SDL_Color get_color(){
-		return color;
+	void AddImage(const char* v_name, Image im) {
+		im_holder.insert(std::make_pair(v_name, im));
 	}
-	void Appear() {
-		image.Load();
+	void DeleteImage(const char* v_name) {
+		im_holder.erase(v_name);
 	}
-	void Disappear() {
-		image.Disappear();
+	void Appear(const char* im_name) {
+		im_holder[im_name].Load();
+	}
+	void Disappear(const char* im_name) {
+		im_holder[im_name].Disappear();
 	}
 };
