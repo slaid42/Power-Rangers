af90648ec65f0e462068a424c195100897dc12a5
Кощей Богдан <71867935+slaid42@users.noreply.github.com>
2021-12-09 03:10:13 +0300

    Update Values_holder.h

--- Yameteeeeee/Values_holder.h
+++ Yameteeeeee/Values_holder.h
@@ -1,56 +1,46 @@
+#pragma once
+#include <unordered_map>
 #include <string>
 #include <variant>
 #include <map>
-#include <iostream>
+#include <fstream>
+
 
 class Value {
 private:
-	std::variant<int, bool, std::string> v;
+	std::variant<int, std::string> v;
+	std::string value_type;
 public:
 	Value() = default;
 	Value(std::string s) {
 		if (Empty()) {
 			v = s;
+			
 		}
 		else {
-			std::variant<int, bool, std::string> v1 = s;
+			std::variant<int, std::string> v1 = s;
 			v = v1;
 		}
-	}
-	Value(bool b) {
-		if (Empty()) {
-			v = b;
-		}
-		else {
-			std::variant<int, bool, std::string> v1 = b;
-			v = v1;
-		}
+		value_type = "string";
 	}
 	Value(int i) {
 		if (Empty()) {
 			v = i;
+
 		}
 		else {
-			std::variant<int, bool, std::string> v1 = i;
+			std::variant<int, std::string> v1 = i;
 			v = v1;
 		}
+		value_type = "int";
 	}
-	
+
 	Value& operator=(std::string& s) {
 		if (Empty()) {
 			v = s;
 		}
 		else {
-			std::variant<int, bool, std::string> v1 = s;
-			v = v1;
-		}
-	}
-	Value& operator=(bool& b) {
-		if (Empty()) {
-			v = b;
-		}
-		else {
-			std::variant<int, bool, std::string> v1 = b;
+			std::variant<int, std::string> v1 = s;
 			v = v1;
 		}
 	}
@@ -59,32 +49,30 @@ public:
 			v = i;
 		}
 		else {
-			std::variant<int, bool, std::string> v1 = i;
+			std::variant<int, std::string> v1 = i;
 			v = v1;
 		}
 	}
 	int GetInt() {
 		return std::get<int>(v);
 	}
-	bool GetBool() {
-		return std::get<bool>(v);
-	}
 	std::string GetString() {
 		return std::get<std::string>(v);
 	}
 	bool Empty() {
 		int* i = std::get_if<int>(&v);
-		bool* b = std::get_if<bool>(&v);
-		
 		std::string* s = std::get_if<std::string>(&v);
-		if (i == nullptr && b == nullptr && s == nullptr) {
+		if (i == nullptr && s == nullptr) {
 			return true;
 		}
 		else {
-			
+
 			return false;
 		}
 
+	}
+	std::string Type() {
+		return value_type;
 	}
 };
 
@@ -93,14 +81,19 @@ public:
 class ValuesHolder {
 private:
 	std::map<const char*, Value> m;
+	const char* directive;
 public:
-	ValuesHolder() = default;
+	ValuesHolder(const char* _directive) {
+		directive = _directive;
+		Read();
+	};
 	ValuesHolder(const ValuesHolder& other) = delete;
 	ValuesHolder(ValuesHolder& other) = delete;
 	ValuesHolder& operator= (const ValuesHolder& other) = delete;
 	ValuesHolder& operator= (ValuesHolder& other) = delete;
 	void AddValue(const char* v_name, Value val) {
 		m.insert(std::make_pair(v_name, val));
+
 	}
 	Value FindValue(const char* v_name) {
 		return m[v_name];
@@ -110,5 +103,43 @@ public:
 	}
 	void DeleteValue(const char* v_name) {
 		m.erase(v_name);
+	}
+	void Read() {
+		std::ifstream in(directive); // окрываем файл для чтения
+		std::string name;
+		std::string val;
+		std::string indicator;
+		while (in >> indicator >> name >> val)
+		{	
+			if (indicator == "int") {
+				const char* v_name = name.c_str();
+				int value = stoi(val);
+				Value _value(value);
+				m.insert(std::make_pair(v_name, _value));
+			}
+			if (indicator == "string") {
+				const char* v_name = name.c_str();
+				Value _value(val);
+				m.insert(std::make_pair(v_name, _value));
+			}
+		}
+		in.close();
+	}
+	void Write() {
+		std::ofstream out(directive);
+		if (out.is_open())
+		{
+			out << "Welcome to CPP" << std::endl;
+		}
+
+		for (auto i = m.begin(); i != m.end(); ++i) {
+			if (((*i).second).Type() == "int") {
+				out << "int" << " " << (*i).first << " " << ((*i).second).GetInt() << std::endl;
+			}
+			if (((*i).second).Type() == "string") {
+				out << "string" << " " << (*i).first << " " << ((*i).second).GetString() << std::endl;
+			}
+		}
+		out.close();
 	}
 };
